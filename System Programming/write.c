75abcc3c2ee99fe0085885c7b2ff60bdc78843d0
star <juniorprogrammer09@gmail.com>
2022-12-02 07:26:40 +0300

    write system call initial

---
+++ System Programming/write.c
@@ -0,0 +1,24 @@
+/***
+* A program to demonstrate the write system call
+* used ti write to a file descriptor
+* syntax   ssize_t write(int fd,const void * buff, size_t count);
+* returns the size it was able to write
+
+* # File descriptors[fd] are three 0,1,2
+*  0-standard input device e.g. keyboard
+*  1-standard output device e.g. screen
+*  2-standard error
+
+*/
+
+#include<stdio.h>
+#include <unistd.h>
+
+int main(){
+
+
+
+return 0;
+}
+
+
