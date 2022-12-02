75ac490d60730fe88cbb3474543e1c380ce421c6
star <juniorprogrammer09@gmail.com>
2022-12-02 07:36:17 +0300

    initial pipe implementation

---
+++ System Programming/pipe.c
@@ -0,0 +1,15 @@
+/***
+* A program to demonstrate the pipe system call
+* used to allow the output of one process to be used as the input of another process
+
+*/
+
+#include<stdio.h>
+#include <unistd.h>
+
+int main(){
+
+return 0;
+}
+
+
