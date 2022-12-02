365f3eb10e3e6a5d7c91c0ed32c8f0b7925d1936
star <juniorprogrammer09@gmail.com>
2022-12-02 07:36:53 +0300

    initial share memory implementation

---
+++ System Programming/shared_memory.c
@@ -0,0 +1,17 @@
+/***
+* A program to demonstrate the shared memory system calls
+* used to allocate a shared memory to different processes
+* syntax  
+*/
+
+#include<stdio.h>
+#include <unistd.h>
+
+int main(){
+
+
+
+return 0;
+}
+
+
