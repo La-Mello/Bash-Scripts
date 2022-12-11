105cdca5e629f9ad7a40902e82dc08cd2ca4af0e
star <juniorprogrammer09@gmail.com>
2022-12-11 12:58:20 +0300

    zombie process final

--- System Programming/zombie_process.c
+++ System Programming/zombie_process.c
@@ -1,14 +1,18 @@
 /**
 	How to create a zombie process
 	Zombie process -> when a child process terminates without notifying the parent
+	parent assumes that child still exist in system while it doesn't
+
+# run ./zombie &
+# after running the ps cmd the defunct is the zombie process
+
+#how to avoid zombie process use wait() instead of sleep()
 
 **/
 #include<stdio.h>
 #include<sys/types.h>
 #include<sys/wait.h>
 #include<unistd.h>
-
-
 
 int main(){
 
@@ -25,7 +29,9 @@ int main(){
 		printf("My parent Pid<%d>\n",getppid());
 
 	}else{
-
+		//sleep(3);//makes the parent sleep for 3 seconds to allow child to finish first
+		wait(NULL); 
+		sleep(3);//to allow use of ps
 		printf("Parent process pid<%d>\n",getpid());
 		printf("My child Pid<%d>\n",pid);
 
